5990e97219ec45ab556dcdfbd92f9e32177360e3
Tín Tạ-Trung <tatrungtin2004@gmail.com>
2024-09-29 10:44:20 +0000

    Pass all testcases

--- controller.js
+++
@@ -1,7 +0,0 @@
-export function uploadFile(req, res) {
-    res.json({
-        name: req.file.originalname,
-        type: req.file.mimetype,
-        size: req.file.size
-    });
-}
--- index.js
+++ index.js
@@ -2,17 +2,42 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'; dotenv.config();
 import { uploadFile } from './controller.js'
-
+import multer from 'multer';
+import path from 'path';
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// set up multer storage engine
+const storage = multer.diskStorage({
+  destination: "./uploads/",
+  filename: function(req, file, cb) {
+    cb(null, file.originalname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Initialize multer Middleware
+const upload = multer({ storage: storage}).single('upfile');
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse', uploadFile);
+app.post('/api/fileanalyse', function(req, res) {
+  upload(req, res, function(err) {
+    if (err) {
+      console.error(err);
+      res.send("Error uploading file!");
+    }
+    console.log(req.file);
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    })
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
